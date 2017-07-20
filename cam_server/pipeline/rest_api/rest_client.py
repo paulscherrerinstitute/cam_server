8d156f5b2b2092a0d941db63970db33ad4ad2b48
Andrej Babic <andrej.babic@psi.ch>
2017-07-20 11:01:24 +0200

    Create pipeline client

--- cam_server/pipeline/rest_api/rest_client.py
+++ cam_server/pipeline/rest_api/rest_client.py
@@ -3,13 +3,13 @@ import requests
 from cam_server import config
 
 
-class CamClient(object):
+class PipelineClient(object):
     def __init__(self, address="http://0.0.0.0:8888/"):
         """
         :param address: Address of the cam API, e.g. http://localhost:10000
         """
 
-        self.api_address_format = address.rstrip("/") + config.API_PREFIX + "/%s"
+        self.api_address_format = address.rstrip("/") + config.API_PREFIX + config.PIPELINE_REST_INTERFACE_PREFIX + "%s"
 
     def get_server_info(self):
         """
@@ -17,7 +17,7 @@ class CamClient(object):
         For administrative purposes only.
         :return: Status of the server
         """
-        rest_endpoint = "cam_server/info"
+        rest_endpoint = "/info"
         return requests.get(self.api_address_format % rest_endpoint).json()
 
     def get_cameras(self):
@@ -25,7 +25,7 @@ class CamClient(object):
         List existing cameras.
         :return: Currently existing cameras.
         """
-        rest_endpoint = "cam_server"
+        rest_endpoint = ""
         return requests.get(self.api_address_format % rest_endpoint).json()
 
     def get_camera_config(self, camera_name):
@@ -34,7 +34,7 @@ class CamClient(object):
         :param camera_name: Name of the cam_server.
         :return: Camera configuration.
         """
-        rest_endpoint = "cam_server/%s/config" % camera_name
+        rest_endpoint = "/%s/config" % camera_name
         return requests.get(self.api_address_format % rest_endpoint).json()
 
     def set_camera_config(self, camera_name, config):
@@ -44,7 +44,7 @@ class CamClient(object):
         :param config: Config to set, in dictionary format.
         :return: Actual applied config.
         """
-        rest_endpoint = "cam_server/%s" % camera_name
+        rest_endpoint = "/%s" % camera_name
         return requests.post(self.api_address_format % rest_endpoint, json=config).json()
 
     def get_camera_geometry(self, camera_name):
@@ -53,7 +53,7 @@ class CamClient(object):
         :param camera_name: Name of the cam_server.
         :return: Camera geometry.
         """
-        rest_endpoint = "cam_server/%s/geometry" % camera_name
+        rest_endpoint = "/%s/geometry" % camera_name
         return requests.get(self.api_address_format % rest_endpoint).json()
 
     def get_camera_image(self, camera_name):
@@ -62,7 +62,7 @@ class CamClient(object):
         :param camera_name: Camera name.
         :return: Response content (PNG).
         """
-        rest_endpoint = "cam_server/%s/image" % camera_name
+        rest_endpoint = "/%s/image" % camera_name
         return requests.get(self.api_address_format % rest_endpoint).content
 
     def get_camera_stream(self, camera_name):
@@ -71,7 +71,7 @@ class CamClient(object):
         :param camera_name: Name of the camera to get the address for.
         :return: Stream address.
         """
-        rest_endpoint = "cam_server/%s" % camera_name
+        rest_endpoint = "/%s" % camera_name
         return requests.get(self.api_address_format % rest_endpoint).json()
 
     def stop_camera(self, camera_name):
@@ -80,7 +80,7 @@ class CamClient(object):
         :param camera_name: Name of the camera to stop.
         :return: Response.
         """
-        rest_endpoint = "cam_server/%s" % camera_name
+        rest_endpoint = "/%s" % camera_name
         return requests.delete(self.api_address_format % rest_endpoint).json()
 
     def stop_all_cameras(self):
@@ -88,6 +88,6 @@ class CamClient(object):
         Stop all the cameras on the server.
         :return: Response.
         """
-        rest_endpoint = "cam_server"
+        rest_endpoint = ""
         return requests.delete(self.api_address_format % rest_endpoint).json()
 
