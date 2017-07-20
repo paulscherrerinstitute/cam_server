e9f22e02c3988f0d8944dc07bea828d04f00d7c6
Andrej Babic <andrej.babic@psi.ch>
2017-07-20 10:58:17 +0200

    Add pipeline server

--- cam_server/start.py
+++ cam_server/start.py
@@ -4,9 +4,12 @@ import os
 
 import bottle
 
+from cam_server.pipeline.manager import PipelineInstanceManager
+from cam_server.pipeline.rest_api.rest_server import register_rest_interface as register_pipeline_rest_interface
+
 from cam_server import config
 from cam_server.camera.manager import CameraConfigManager, CameraInstanceManager, CameraConfigFileStorage
-from cam_server.rest_api.rest_server import register_rest_interface as register_camera_rest_interface
+from cam_server.camera.rest_api.rest_server import register_rest_interface as register_camera_rest_interface
 
 _logger = logging.getLogger(__name__)
 
@@ -19,16 +22,20 @@ def start_camera_server(host, port, config_base):
         exit(-1)
 
     config_manager = CameraConfigManager(config_provider=CameraConfigFileStorage(config_base))
-    instance_manager = CameraInstanceManager(config_manager)
+    camera_instance_manager = CameraInstanceManager(config_manager)
+    pipeline_instance_manager = PipelineInstanceManager(config_manager)
 
     app = bottle.Bottle()
-    register_camera_rest_interface(app=app, instance_manager=instance_manager)
+
+    register_camera_rest_interface(app=app, instance_manager=camera_instance_manager)
+    register_pipeline_rest_interface(app=app, instance_manager=pipeline_instance_manager)
 
     try:
         bottle.run(app=app, host=host, port=port)
     finally:
         # Close the external processor when terminating the web server.
-        instance_manager.stop_all_cameras()
+        camera_instance_manager.stop_all_instances()
+        pipeline_instance_manager.stop_all_instances()
 
 
 if __name__ == "__main__":
