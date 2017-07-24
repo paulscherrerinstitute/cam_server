e8580f6b84c14145f942c53db4ea6cc86f63f9dc
Andrej Babic <andrej.babic@psi.ch>
2017-07-24 14:00:53 +0200

    Add simulated camera config test

--- tests/test_camera_config.py
+++ tests/test_camera_config.py
@@ -5,12 +5,19 @@ from tests.helpers.camera import get_test_instance_manager
 
 class CameraConfigTest(unittest.TestCase):
 
-    def test_get_geometry(self):
-        width, height = get_test_instance_manager().config_manager.get_camera_geometry("simulation")
+    def test_simulation_camera(self):
+        instance_manager = get_test_instance_manager()
+
+        cameras = instance_manager.get_camera_list()
+        self.assertListEqual(cameras, ["simulation"], "Simulation camera missing in config.")
+
+        width, height = instance_manager.config_manager.get_camera_geometry("simulation")
         self.assertTrue(width > 0 and height > 0, "Width and height need to be positive numbers.")
 
+        configuration = instance_manager.config_manager.get_camera_config("simulation")
+        self.assertIsNotNone(configuration, "Configuration for the simulated camera should exist.")
+
     def test_get_camera_config(self):
-        # TODO: Test the config.
         pass
 
     def test_set_camera_config(self):
