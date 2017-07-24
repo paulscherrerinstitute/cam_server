ef4a6a4d08cdc9d689b13d4f31a582a0565d8378
Andrej Babic <andrej.babic@psi.ch>
2017-07-24 10:39:34 +0200

    Fix typo

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -10,30 +10,30 @@ class CameraTest(unittest.TestCase):
 
     def test_get_stream(self):
 
-        instance_mananger = get_test_instance_manager()
+        instance_manager = get_test_instance_manager()
 
-        self.assertListEqual([self.simulation_camera], instance_mananger.get_camera_list(),
+        self.assertListEqual([self.simulation_camera], instance_manager.get_camera_list(),
                              "Missing simulation camera.")
 
         # Shorten the time to make the tests run faster.
         old_timeout = config.MFLOW_NO_CLIENTS_TIMEOUT
         config.MFLOW_NO_CLIENTS_TIMEOUT = 1
 
-        stream_address = instance_mananger.get_camera_stream("simulation")
-        stream_address_copy = instance_mananger.get_camera_stream("simulation")
+        stream_address = instance_manager.get_camera_stream("simulation")
+        stream_address_copy = instance_manager.get_camera_stream("simulation")
 
         # We should get the same stream both times.
         self.assertEqual(stream_address, stream_address_copy,
                          "Got 2 stream addresses, instead of the same one twice.")
 
-        n_active_instances = len(instance_mananger.get_info()["active_instances"])
+        n_active_instances = len(instance_manager.get_info()["active_instances"])
         self.assertTrue(n_active_instances == 1, "Number of active instances is not correct.")
 
         # Lets wait for the stream to dies.
         sleep(config.MFLOW_NO_CLIENTS_TIMEOUT + 1.5)
 
         # The simulation camera should disconnect, since the no client timeout has passed.
-        n_active_instances = len(instance_mananger.get_info()["active_instances"])
+        n_active_instances = len(instance_manager.get_info()["active_instances"])
         self.assertTrue(n_active_instances == 0, "All instances should be dead by now.")
 
         # Restore the old timeout.
