23c9fbc71ce833dbfb3cb1f0dc0e83cd59d173b5
Andrej Babic <andrej.babic@psi.ch>
2017-07-24 11:21:36 +0200

    Add placeholders for tests

--- tests/test_camera_config.py
+++ tests/test_camera_config.py
@@ -8,3 +8,11 @@ class CameraConfigTest(unittest.TestCase):
     def test_get_geometry(self):
         width, height = get_test_instance_manager().config_manager.get_camera_geometry("simulation")
         self.assertTrue(width > 0 and height > 0, "Width and height need to be positive numbers.")
+
+    def test_get_camera_config(self):
+        # TODO: Test the config.
+        pass
+
+    def test_set_camera_config(self):
+        # TODO: Test the config.
+        pass
