b9ed0f01f8ba6d95215223048f457dd8dd54a282
Andrej Babic <andrej.babic@psi.ch>
2017-07-26 11:19:45 +0200

    Fix camera receiver test

--- tests/test_camera_receiver.py
+++ tests/test_camera_receiver.py
@@ -8,17 +8,23 @@ class CameraReceiverTest(unittest.TestCase):
     def test_camera_simulation(self):
         camera = CameraSimulation()
 
-        def callback_method(image, timestamp):
+        n_images_to_receive = 5
 
+        def callback_method(image, timestamp):
             self.assertIsNotNone(image, "Image should not be None")
             self.assertIsNotNone(timestamp, "Timestamp should not be None")
 
-            camera.clear_callbacks()
-            camera.disconnect()
+            nonlocal n_images_to_receive
+            if n_images_to_receive <= 0:
+                camera.clear_callbacks()
+                camera.simulation_stop_event.set()
 
-        camera.add_callback(callback_method)
+            n_images_to_receive -= 1
+
         camera.connect()
+        camera.add_callback(callback_method)
 
+        camera.simulation_stop_event.wait()
 
 if __name__ == '__main__':
     unittest.main()
