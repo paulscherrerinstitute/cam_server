0cfd77d08194e11fc8e3b431601e7dee23da7541
Andrej Babic <andrej.babic@psi.ch>
2017-07-26 11:19:35 +0200

    Fix utils method

--- cam_server/utils.py
+++ cam_server/utils.py
@@ -3,4 +3,4 @@ def get_host_port_from_stream_address(stream_address):
     source_host, source_port = stream_address.rsplit(":", maxsplit=1)
     source_host = source_host.split("//")[1]
 
-    return source_host, source_port
+    return source_host, int(source_port)
