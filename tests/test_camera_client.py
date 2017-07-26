8ffcf6ac118aa4167e09decbc313d25eb9803b8e
Andrej Babic <andrej.babic@psi.ch>
2017-07-26 13:15:29 +0200

    Add client tests

--- tests/test_camera_client.py
+++ tests/test_camera_client.py
@@ -1,4 +1,78 @@
+import os
+import signal
 import unittest
+
+from multiprocessing import Process
+from time import sleep
+
+from bsread import source
+
+from cam_server import CamClient
+from cam_server.start import start_camera_server
+from cam_server.utils import get_host_port_from_stream_address
+
+
+class CameraClientTest(unittest.TestCase):
+    def setUp(self):
+        self.host = "0.0.0.0"
+        self.port = 8888
+
+        test_base_dir = os.path.split(os.path.abspath(__file__))[0]
+        config_folder = os.path.join(test_base_dir, "test_camera_config/")
+
+        self.process = Process(target=start_camera_server, args=(self.host, self.port, config_folder))
+        self.process.start()
+
+        # Give it some time to start.
+        sleep(0.5)
+
+        server_address = "http://%s:%s" % (self.host, self.port)
+        self.client = CamClient(server_address)
+
+    def tearDown(self):
+        os.kill(self.process.pid, signal.SIGINT)
+        # Wait for the server to die.
+        sleep(1)
+
+    def test_client(self):
+        server_info = self.client.get_server_info()
+
+        self.assertIsNot(server_info["active_instances"],
+                         "There should be no running instances.")
+
+        expected_cameras = ["example_1", "example_2", "example_3", "example_4", "simulation"]
+
+        self.assertListEqual(self.client.get_cameras(), expected_cameras, "Not getting all expected cameras")
+
+        camera_stream_address = self.client.get_camera_stream("simulation")
+
+        self.assertTrue(bool(camera_stream_address), "Camera stream address cannot be empty.")
+
+        self.assertTrue("simulation" in self.client.get_server_info()["active_instances"],
+                        "Simulation camera not present in server info.")
+
+        # Check if we can connect to the stream and receive data (in less than 2 seconds).
+        host, port = get_host_port_from_stream_address(camera_stream_address)
+        with source(host=host, port=port, receive_timeout=2000) as stream:
+            data = stream.receive()
+            self.assertIsNotNone(data, "Received data was none.")
+
+        # Stop the simulation instance.
+        self.client.stop_camera("simulation")
+
+        self.assertTrue("simulation" not in self.client.get_server_info()["active_instances"],
+                        "Camera simulation did not stop.")
+
+        self.client.get_camera_stream("simulation")
+
+        self.assertTrue("simulation" in self.client.get_server_info()["active_instances"],
+                        "Camera simulation did not start.")
+
+        self.client.stop_all_cameras()
+
+        self.assertTrue("simulation" not in self.client.get_server_info()["active_instances"],
+                        "Camera simulation did not stop.")
+
 
 if __name__ == '__main__':
     unittest.main()
--- tests/test_camera_manager.py
+++ tests/test_camera_manager.py
@@ -13,84 +13,86 @@ from tests.helpers.camera import get_test_instance_manager
 class CameraTest(unittest.TestCase):
     simulation_camera = "simulation"
 
+    def setUp(self):
+        self.instance_manager = get_test_instance_manager()
+
+    def tearDown(self):
+        self.instance_manager.stop_all_instances()
+        sleep(1)
+
     def test_get_stream(self):
 
-        instance_manager = get_test_instance_manager()
-
-        self.assertListEqual([self.simulation_camera], instance_manager.get_camera_list(),
+        self.assertListEqual([self.simulation_camera], self.instance_manager.get_camera_list(),
                              "Missing simulation camera.")
 
         # Shorten the time to make the tests run faster.
         old_timeout = config.MFLOW_NO_CLIENTS_TIMEOUT
         config.MFLOW_NO_CLIENTS_TIMEOUT = 1
 
-        stream_address = instance_manager.get_camera_stream("simulation")
-        stream_address_copy = instance_manager.get_camera_stream("simulation")
+        stream_address = self.instance_manager.get_camera_stream("simulation")
+        stream_address_copy = self.instance_manager.get_camera_stream("simulation")
 
         # We should get the same stream both times.
         self.assertEqual(stream_address, stream_address_copy,
                          "Got 2 stream addresses, instead of the same one twice.")
 
-        n_active_instances = len(instance_manager.get_info()["active_instances"])
+        n_active_instances = len(self.instance_manager.get_info()["active_instances"])
         self.assertTrue(n_active_instances == 1, "Number of active instances is not correct.")
 
         # Lets wait for the stream to dies.
         sleep(config.MFLOW_NO_CLIENTS_TIMEOUT + 1)
 
         # The simulation camera should disconnect, since the no client timeout has passed.
-        n_active_instances = len(instance_manager.get_info()["active_instances"])
+        n_active_instances = len(self.instance_manager.get_info()["active_instances"])
         self.assertTrue(n_active_instances == 0, "All instances should be dead by now.")
 
         # Restore the old timeout.
         config.MFLOW_NO_CLIENTS_TIMEOUT = old_timeout
 
     def test_multiple_stream_requests(self):
-        instance_manager = get_test_instance_manager()
 
         # Shorten the time to make the tests run faster.
         old_timeout = config.MFLOW_NO_CLIENTS_TIMEOUT
         config.MFLOW_NO_CLIENTS_TIMEOUT = 0.5
 
-        stream_address = instance_manager.get_camera_stream(self.simulation_camera)
+        stream_address = self.instance_manager.get_camera_stream(self.simulation_camera)
 
         for _ in range(5):
             # The camera stream address should always be the same.
-            self.assertEqual(stream_address, instance_manager.get_camera_stream(self.simulation_camera))
+            self.assertEqual(stream_address, self.instance_manager.get_camera_stream(self.simulation_camera))
 
         for _ in range(5):
             # The camera stream address should be the same even after restarting the camera instance.
-            self.assertEqual(stream_address, instance_manager.get_camera_stream(self.simulation_camera))
+            self.assertEqual(stream_address, self.instance_manager.get_camera_stream(self.simulation_camera))
 
             sleep(config.MFLOW_NO_CLIENTS_TIMEOUT + 1.5)
 
-            n_active_instances = len(instance_manager.get_info()["active_instances"])
+            n_active_instances = len(self.instance_manager.get_info()["active_instances"])
             self.assertTrue(n_active_instances == 0, "All instances should be dead by now.")
 
         # Restore the old timeout.
         config.MFLOW_NO_CLIENTS_TIMEOUT = old_timeout
 
     def test_stop_stream(self):
-        instance_manager = get_test_instance_manager()
 
         old_timeout = config.MFLOW_NO_CLIENTS_TIMEOUT
         # We need a high timeout to be sure the stream does not disconnect by itself.
         config.MFLOW_NO_CLIENTS_TIMEOUT = 30
 
-        instance_manager.get_camera_stream(self.simulation_camera)
-        self.assertTrue(self.simulation_camera in instance_manager.get_info()["active_instances"],
+        self.instance_manager.get_camera_stream(self.simulation_camera)
+        self.assertTrue(self.simulation_camera in self.instance_manager.get_info()["active_instances"],
                         "Simulation camera instance is not running.")
 
-        instance_manager.stop_instance(self.simulation_camera)
-        self.assertTrue(self.simulation_camera not in instance_manager.get_info()["active_instances"],
+        self.instance_manager.stop_instance(self.simulation_camera)
+        self.assertTrue(self.simulation_camera not in self.instance_manager.get_info()["active_instances"],
                         "Simulation camera instance should not be running.")
 
         # Revert back.
         config.MFLOW_NO_CLIENTS_TIMEOUT = old_timeout
 
     def test_camera_image(self):
-        instance_manager = get_test_instance_manager()
 
-        camera = instance_manager.config_manager.load_camera(self.simulation_camera)
+        camera = self.instance_manager.config_manager.load_camera(self.simulation_camera)
 
         # Retrieve a single image from the camera.
         camera.connect()
