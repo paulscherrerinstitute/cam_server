3e14bea907751ad41b584fee1250cf0bd12111bb
Andrej Babic <andrej.babic@psi.ch>
2017-08-08 17:26:43 +0200

    Width and height fix release

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=0.0.5
+VERSION=0.0.6
 docker build --no-cache=true -t docker.psi.ch:5000/cam_server .
 docker tag docker.psi.ch:5000/cam_server docker.psi.ch:5000/cam_server:$VERSION
 docker push docker.psi.ch:5000/cam_server:$VERSION
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="0.0.5",
+      version="0.0.6",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
