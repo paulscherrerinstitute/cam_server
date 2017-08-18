993d95738b6979801489db03ec008f1015cd89e4
Andrej Babic <andrej.babic@psi.ch>
2017-08-18 16:22:27 +0200

    New production release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="1.0.3",
+      version="1.1.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
