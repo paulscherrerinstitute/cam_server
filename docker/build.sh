47dc40604e5ce7136bbe1526a90470826bf8286c
Andrej Babic <andrej.babic@psi.ch>
2017-08-21 15:24:37 +0200

    Increase library version for production

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=1.1.1
+VERSION=1.1.2
 docker build --no-cache=true -t docker.psi.ch:5000/cam_server .
 docker tag docker.psi.ch:5000/cam_server docker.psi.ch:5000/cam_server:$VERSION
 docker push docker.psi.ch:5000/cam_server:$VERSION
