0401a2a58bc7f23d15404bc6725d3012576fb5c7
Andrej Babic <andrej.babic@psi.ch>
2017-08-22 17:49:16 +0200

    New major release
    Since the configuration of the calibration was migrated from the
    pipeline server into the camera server, the interface has
    changed and consequently a major version release is due.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="1.1.2",
+      version="2.0.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
