53b0381a9cdba4abacfdb4984f53aa32604768ee
Andrej Babic <andrej.babic@psi.ch>
2017-09-28 16:42:34 +0200

    New library version
    - Timestamp propagation

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.2.1
+VERSION=2.2.2
 docker build --no-cache=true -t docker.psi.ch:5000/cam_server .
 docker tag docker.psi.ch:5000/cam_server docker.psi.ch:5000/cam_server:$VERSION
 docker push docker.psi.ch:5000/cam_server:$VERSION
