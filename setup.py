6b8dc547491ced7a5004c0aab0f6b5c7f0c510f5
Andrej Babic <andrej.babic@psi.ch>
2017-10-26 14:51:24 +0200

    New library version
    - Good region intensity

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="2.2.3",
+      version="2.2.4",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
