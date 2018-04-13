ae193a8bb1b5b4c56305aeb995ccfebdaec323ea
Andrej Babic <andrej.babic@psi.ch>
2018-04-13 14:25:16 +0200

    New library version
    - Connection to ZMQ stream.
    - Bsread fixes.
    - Pipeline fixes (fit).

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.2.7
+VERSION=2.3.0
 docker build --no-cache=true -t docker.psi.ch:5000/cam_server .
 docker tag docker.psi.ch:5000/cam_server docker.psi.ch:5000/cam_server:$VERSION
 docker push docker.psi.ch:5000/cam_server:$VERSION
