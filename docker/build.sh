fe8408c85f99a6b4e5fe5e48403729e8d9f22d75
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-04-16 14:35:45 +0200

    Version 3.0: First proxy implementation (semi-transparent)

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=2.5.1
+VERSION=3.0.0
 docker build --no-cache=true -t paulscherrerinstitute/cam_server .
 docker tag paulscherrerinstitute/cam_server paulscherrerinstitute/cam_server:$VERSION
 docker login
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="2.5.1",
+      version="3.0.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
