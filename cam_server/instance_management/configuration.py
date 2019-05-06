1295b184f961e8ada3b54b578375e1c783420490
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-05-06 16:50:26 +0200

    Bug fix

--- cam_server/instance_management/configuration.py
+++ cam_server/instance_management/configuration.py
@@ -165,12 +165,13 @@ def get_proxy_config(config_base, config_str):
     if not config_str:
         with open(config_base + "/servers.json") as data_file:
             configuration = json.load(data_file)
-    config_str = config_str.strip()
-    # json
-    if config_str.startswith("{"):
-        configuration = json.loads(config_str)
     else:
-        configuration = {}
-        for server in [s.strip() for s in config_str.split(",")]:
-            configuration[server] = {"expanding": True}
+        config_str = config_str.strip()
+        # json
+        if config_str.startswith("{"):
+            configuration = json.loads(config_str)
+        else:
+            configuration = {}
+            for server in [s.strip() for s in config_str.split(",")]:
+                configuration[server] = {"expanding": True}
     return configuration
