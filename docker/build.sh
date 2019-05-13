15e949cf2e39973c7a6f5c9237d81098f7125ff5
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-05-13 11:46:05 +0200

    Version 3.0.4

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=3.0.3
+VERSION=3.0.4
 docker build --no-cache=true -t paulscherrerinstitute/cam_server .
 docker tag paulscherrerinstitute/cam_server paulscherrerinstitute/cam_server:$VERSION
 docker login
