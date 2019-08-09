69136a3af3b0494caac3fb03062ab9e431947db4
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-08-09 10:00:55 +0200

    Version 3.2.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 
 
 setup(name="cam_server",
-      version="3.2.0",
+      version="3.2.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
