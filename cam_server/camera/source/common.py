2eb84de0d204bc88ee577e3c38b6a31c15a02394
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2019-09-24 14:15:52 +0200

    Version 3.3.9

--- cam_server/camera/source/common.py
+++ cam_server/camera/source/common.py
@@ -9,6 +9,10 @@ def transform_image(image, camera_config):
     if camera_config.parameters["mirror_y"]:
         image = numpy.flipud(image)
 
-    image = numpy.rot90(image, camera_config.parameters["rotate"])
+    if camera_config.parameters["rotate"] != 0:
+        image = numpy.rot90(image, camera_config.parameters["rotate"])
 
-    return numpy.ascontiguousarray(image)
+    if not image.flags['C_CONTIGUOUS']:
+        image = numpy.ascontiguousarray(image)
+
+    return image
