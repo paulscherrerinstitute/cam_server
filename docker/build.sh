9cdf41613293271ce2133cad90f38c3b3efea801
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2020-05-13 09:36:33 +0200

    Changing version to package with bitshuffle 0.3.6  + python 3.7 build

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=3.5.6
+VERSION=3.5.7
 #docker image prune --all --filter "until=4320h"   #delete images older than 6 months
 #docker system prune
 docker build --no-cache=true -t paulscherrerinstitute/cam_server .
