781ce37b2d53fad353c8156fa811a455b3cac751
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2020-08-25 11:27:08 +0200

    Version 3.9.0

--- setup_client.py
+++ setup_client.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="cam_server",
-      version="3.8.9",
+      version="3.9.0",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
