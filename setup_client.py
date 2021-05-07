523c31eb9b750a6d2a55816e105d7c2d6123c4c6
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2021-05-07 16:56:58 +0200

    Version 4.2.3

--- setup_client.py
+++ setup_client.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="cam_server",
-      version="4.2.2",
+      version="4.2.3",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
