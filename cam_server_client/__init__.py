ce90c9e29a9a9b53bcc057b5c3c64d470f1eb44d
Alexandre Gobbo <alexandre.gobbo@psi.ch>
2022-04-06 10:22:33 +0200

    Version 5.1.1

--- cam_server_client/__init__.py
+++ cam_server_client/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "5.1.0"
+__VERSION__ = "5.1.1"
 
 # Import the cam client.
 from cam_server_client.camera_client import CamClient
--- setup_client.py
+++ setup_client.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="cam_server",
-      version="5.1.0",
+      version="5.1.1",
       maintainer="Paul Scherrer Institute",
       maintainer_email="daq@psi.ch",
       author="Paul Scherrer Institute",
