a040dcab07c7ce775d4a88068a71bca3f2fd8173
gobbo_a <alexandre.gobbo@psi.ch>
2024-03-22 11:04:10 +0100

    Timestamp not present in stream when data is  invalid

--- cam_server/pipeline/data_processing/transparent.py
+++ cam_server/pipeline/data_processing/transparent.py
@@ -18,12 +18,14 @@ def process_image(image, pulse_id, timestamp, x_axis, y_axis, parameters, bsdata
     return_value["y_axis"] = y_axis
     return_value["width"] = image.shape[1]
     return_value["height"] = image.shape[0]
-    return_value["timestamp"] =  timestamp_as_float(timestamp)
     return_value["image"] = image
     # Needed for config traceability.
     return_value["processing_parameters"] = json.dumps(parameters)
 
     if parameters.get(PIPELINE_PROCESSING_ERROR, None):
         return_value["image"] = set_invalid_image(return_value["image"])
+        return return_value
+
+    return_value["timestamp"] =  timestamp_as_float(timestamp)
 
     return return_value
