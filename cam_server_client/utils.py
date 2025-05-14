adf550ea3c97c993e6faa82c85df2ba44ca3358f
gobbo_a <alexandre.gobbo@psi.ch>
2025-05-14 10:10:09 +0200

    Optional protocol in address parsing

--- cam_server_client/utils.py
+++ cam_server_client/utils.py
@@ -1,8 +1,8 @@
 def get_host_port_from_stream_address(stream_address):
     if stream_address.startswith("ipc"):
         return stream_address.split("//")[1], -1
-
     source_host, source_port = stream_address.rsplit(":", maxsplit=1)
-    source_host = source_host.split("//")[1]
+    if "//" in source_host:
+        source_host = source_host.split("//")[1]
     return source_host, int(source_port)
 
