743c33b8c952c3b4e0223f123556cc06130cb044
gobbo_a <alexandre.gobbo@psi.ch>
2025-08-11 15:24:14 +0200

    Bump version

--- tests/user_scripts/pipolar.py
+++ tests/user_scripts/pipolar.py
@@ -22,19 +22,18 @@ class PpolPeakValley():
         self.x, self.y = self.ppol_interpol()
 
     def ppol_interpol(self, plot_flag=False):
-        ipv = [0.0, 0.0164, 0.0328, 0.0492, 0.0656,
-               0.082, 0.157, 0.296, 0.42, 0.53, 0.706, 0.833, 0.918, 0.97, 0.995]
-        sig = [0.0,  1.254192, 2.508384, 3.7626, 5.016768,
-               6.27096483804526, 8.86848352312841, 12.5419296760905,
-               15.3606640481458, 17.7369670462568, 21.723259423944,
-               25.083859352181, 28.0446073247603, 30.7213280962917,
-               33.1828268837964]
+        # ipv = [32.98,20.85,15.31,12.12,10.05,8.60,7.52,6.69,6.028,5.49,5.047,4.67,4.35,4.078,3.84,3.44,3.12,2.87,2.65,2.47,2.32]
+        ipv = [0.032771623, 0.045653159, 0.058377743, 0.070933335, 0.083347283, 0.095648417, 0.107858216, 0.119990516,
+               0.132053213, 0.144050065, 0.155982387, 0.167849814, 0.179651077, 0.19138435, 0.203047604, 0.22615558,
+               0.248958238, 0.271440041, 0.293587065, 0.315386908, 0.33682858]
+        sig = [3.66, 5.18, 6.34, 7.32, 8.18, 8.96, 9.68, 10.35, 10.98, 11.57, 12.14, 12.68, 13.19, 13.69, 14.17, 15.09,
+               15.95, 16.77, 17.55, 18.30, 19.014]
         sig2 = [element * element for element in sig]
         x = np.linspace(0, 999999, 999999)
-        x = [(1+val)/1000000*max(ipv) for val in x]
+        x = [(1 + val) / 1000000 * max(ipv) for val in x]
         interpfunc = interpolate.interp1d(ipv, sig2, kind='quadratic')
         inter_x = interpfunc(x)
-        sqrt_val = [math.sqrt(val) for  val in inter_x]
+        sqrt_val = [math.sqrt(val) for val in inter_x]
         y = [0]
         y.extend(sqrt_val)
         y.append(max(sig))
@@ -47,21 +46,20 @@ class PpolPeakValley():
         emittance = 0
         for i in range(0, len(self.x)):
             if self.x[i] > ratio:
-                self.y[i-1]
-                return self.y[i-1]
+                self.y[i - 1]
+                return self.y[i - 1]
 
-#For peak search - delta(h) to max peak value
-DELTA_HEIGHT = 400
-BG_XRANGE_LOW = [340, 400] #100 160
-BG_XRANGE_HIGH = [460, 520] #840 900
 
-PEAK_SEARCH_REL_RANGE = [-1, 2]
+# For peak search - delta(h) to max peak value
+DELTA_HEIGHT = 40000  # 400
+BG_XRANGE_LOW = [0, 200]  # 100 160 [340, 400]
+BG_XRANGE_HIGH = [1452, 1652]  # 840 900 [460, 520]
+
+PEAK_SEARCH_REL_RANGE = [-2, 3]
 VALLEY_SEARCH_REL_RANGE = [-2, 3]
 ppol = PpolPeakValley()
 
-
-
-#abreviations
+# abreviations
 BX1 = BG_XRANGE_LOW[0]
 BX2 = BG_XRANGE_LOW[1]
 BX3 = BG_XRANGE_HIGH[0]
@@ -73,16 +71,15 @@ ydata = []
 
 
 def calculate_emittance(image, fit_pars):
-    global ydata # array of indexes
+    global ydata  # array of indexes
     DELTA_HEIGHT = fit_pars['delta_height']
     BG_XRANGE_LOW = fit_pars['bg_range_low']
     BG_XRANGE_HIGH = fit_pars['bg_range_high']
     PEAK_SEARCH_REL_RANGE = fit_pars['peak_search_rel_range']
     VALLEY_SEARCH_REL_RANGE = fit_pars['valley_search_rel_range']
 
-
-    w,h=len(image[0]),len(image)
-    if len(ydata)!=h:
+    w, h = len(image[0]), len(image)
+    if len(ydata) != h:
         H = []
         for i in range(0, h):
             H.append(i)
@@ -95,127 +92,132 @@ def calculate_emittance(image, fit_pars):
     image -= image.min()
     projy = np.sum(image, axis=1)
     projx = np.sum(image, axis=0)
-    #find peaks
+    # find peaks
     max_element = np.amax(projy)
     max_indices = np.where(projy == max_element)
     peaks, _ = find_peaks(projy, height=(max_element - DELTA_HEIGHT))
 
-    _logger.info("max indices /peaks " + str(max_indices)  + " " + str(peaks))
+    _logger.debug("max indices /peaks " + str(max_indices) + " " + str(peaks))
 
     if len(peaks) != 2:
-            mess = "Too few peaks found! " if len(peaks) < 2 else \
-                   "Too many peaks found "
-            _logger.info(mess + str(peaks))
-            peaks_buffer = []
-            for val in peaks:
-                #if val > 567 and val < 590:
-                    peaks_buffer.append(val)
+        mess = "Too few peaks found! " if len(peaks) < 2 else
+        "Too many peaks found "
+    _logger.debug(mess + str(peaks))
+    peaks_buffer = []
+    for val in peaks:
+        ### COMMENTED BY ALEX
+        # if val > 567 and val < 590:
+        peaks_buffer.append(val)
 
-            #if len(peaks_buffer) ==3:
-            #    peaks = [None] * 2
-            #    peaks[0] = peaks_buffer[0]
-            #    peaks[1] = peaks_buffer[2]
-            if len(peaks_buffer) !=2:
-                return (-1.0, -2.0)
-            else:
-                peaks = peaks_buffer
+    # if len(peaks_buffer) ==3:
+    #    peaks = [None] * 2
+    #    peaks[0] = peaks_buffer[0]
+    #    peaks[1] = peaks_buffer[2]
+    if len(peaks_buffer) != 2:
+        return (-1.0, -2.0)
+    else:
+        peaks = peaks_buffer
 
-    if (peaks[1] - peaks[0]) < 6:
-        _logger.info("Peaks are too close: " + str(peaks[1] - peaks[0]))
-        raise Exception("Peaks are too close")
 
-    #peaks =[569, 577]
-    #Distance to minimum
-    min_element = np.amin(projy[peaks[0]:peaks[1]])
-    min_indices = np.where(projy == min_element)
+if (peaks[1] - peaks[0]) < 6:
+    _logger.debug("Peaks are too close: " + str(peaks[1] - peaks[0]))
+    raise Exception("Peaks are too close")
 
-    min_idx_value = 0 #min_indices[0][0]
-    for val in min_indices[0]:
-        if val > peaks[0] and val < peaks[1]:
-             min_idx_value = val
-             break
+# peaks =[569, 577]
+# Distance to minimum
+min_element = np.amin(projy[peaks[0]:peaks[1]])
+min_indices = np.where(projy == min_element)
 
-    if min_idx_value == 0:
-        raise Exception("min_idx_value == 0")
+min_idx_value = 0  # min_indices[0][0]
+for val in min_indices[0]:
+    if val > peaks[0] and val < peaks[1]:
+        min_idx_value = val
+        break
 
-    for i in range (0, len(peak_array)):
-        peak_array[i] = ydata[peaks[i]+plr[0] : peaks[i]+plr[1]]
-    valley_array = ydata[min_idx_value+vlr[0] : min_idx_value+vlr[1]]
+if min_idx_value == 0:
+    raise Exception("min_idx_value == 0")
 
-    #print("peaks", peaks, flush=True)
-    #print(np.subtract(peaks, h)*(-1))
-    #print("projections peak, valley", projy[(peaks[0]-1):(peaks[1]+2)], projy[valley_array])
-    #print(peak_array, valley_array, flush=True)
-    #x_bg_center = min_indices[0][0]
+for i in range(0, len(peak_array)):
+    peak_array[i] = ydata[peaks[i] + plr[0]: peaks[i] + plr[1]]
+valley_array = ydata[min_idx_value + vlr[0]: min_idx_value + vlr[1]]
 
-    #background
-    bg_y1 = projy[BX1 : BX2]
-    bg_y2 = projy[BX3 : BX4]
+# print("peaks", peaks, flush=True)
+# print(np.subtract(peaks, h)*(-1))
+# print("projections peak, valley", projy[(peaks[0]-1):(peaks[1]+2)], projy[valley_array])
+# print(peak_array, valley_array, flush=True)
+# x_bg_center = min_indices[0][0]
 
-    bg_yS = np.concatenate((bg_y1, bg_y2))
-    bg_xS = list(range(BX1, BX2)) + list(range(BX3, BX4))
+# background
+bg_y1 = projy[BX1: BX2]
+bg_y2 = projy[BX3: BX4]
 
-    bg_x = []
-    bg_y = []
+bg_yS = np.concatenate((bg_y1, bg_y2))
+bg_xS = list(range(BX1, BX2)) + list(range(BX3, BX4))
 
-    for x, y in zip(bg_xS, bg_yS):
-        if y > 800 and y < 1000:
-            bg_x.append(x)
-            bg_y.append(y)
+bg_x = []
+bg_y = []
 
-    #print(bg_x, flush=True)
-    #print(bg_y, flush=True)
-    #fit
-    poly_bg = np.polyfit(bg_x, bg_y, deg=1)
-    array_bg = np.linspace(0, h, 10400)
-    val_bg = np.polyval(poly_bg, array_bg)
+for x, y in zip(bg_xS, bg_yS):
+    ### COMMENTED BY ALEX
+    # if y > 800 and y < 1000:
+    bg_x.append(x)
+    bg_y.append(y)
 
-    for i in range (0, len(proj_peak_array)):
-        proj_peak_array[i] = projy[peaks[i]+plr[0] : peaks[i]+plr[1]]
-    #proj_peak_array[1] = projy[peaks[1]-1 : peaks[1]+2]
-    proj_valley = projy[min_idx_value+vlr[0] : min_idx_value+vlr[1]]
+# print(bg_x, flush=True)
+# print(bg_y, flush=True)
+# fit
+poly_bg = np.polyfit(bg_x, bg_y, deg=1)
+array_bg = np.linspace(0, h, 10400)
+val_bg = np.polyval(poly_bg, array_bg)
 
-    #peaks
-    for i in range(0, 2):
-        poly = np.polyfit(peak_array[i], proj_peak_array[i], deg=2)
-        idx = -poly[1]/2/poly[0]
-        peak_value[i] = np.polyval(poly, idx)
-        peak_bg[i] = val_bg[int(idx)]
+for i in range(0, len(proj_peak_array)):
+    proj_peak_array[i] = projy[peaks[i] + plr[0]: peaks[i] + plr[1]]
+# proj_peak_array[1] = projy[peaks[1]-1 : peaks[1]+2]
+proj_valley = projy[min_idx_value + vlr[0]: min_idx_value + vlr[1]]
 
-    #valley
-    poly2 = np.polyfit(valley_array, proj_valley, deg=2)
-    #Only works for deg=2
-    minv_idx = -poly2[1]/2/poly2[0]
+# peaks
+for i in range(0, 2):
+    poly = np.polyfit(peak_array[i], proj_peak_array[i], deg=2)
+    idx = -poly[1] / 2 / poly[0]
+    peak_value[i] = np.polyval(poly, idx)
+    peak_bg[i] = val_bg[int(idx)]
 
-    poly = np.polyfit(valley_array, proj_valley, deg=4)
+# valley
+poly2 = np.polyfit(valley_array, proj_valley, deg=2)
+# Only works for deg=2
+minv_idx = -poly2[1] / 2 / poly2[0]
 
-    valley_subarray = np.linspace(valley_array[0], valley_array[-1], 800)
-    poly_array =  np.polyval(poly, valley_subarray)
-    valley_fitted_value = min(poly_array)
+poly = np.polyfit(valley_array, proj_valley, deg=4)
 
-    #print("peak value", peak_value, "valley_fitted value", valley_fitted_value, flush=True)
+valley_subarray = np.linspace(valley_array[0], valley_array[-1], 800)
+poly_array = np.polyval(poly, valley_subarray)
+valley_fitted_value = min(poly_array)
 
-    valley_bg = val_bg[int(minv_idx)]
-    #print("valley background", valley_bg, "peak background", peak_bg[0], peak_bg[1], flush=True)
+# print("peak value", peak_value, "valley_fitted value", valley_fitted_value, flush=True)
 
-    if valley_fitted_value <  valley_bg:
-        valley_fitted_value = min( projy[valley_array])
-        if valley_fitted_value <  valley_bg:
-            raise Exception("valley_fitted_value <  valley_bg")
-            #return
+valley_bg = val_bg[int(minv_idx)]
+# print("valley background", valley_bg, "peak background", peak_bg[0], peak_bg[1], flush=True)
 
-    ratio_corrected = 2*(valley_fitted_value-valley_bg)/(
-        abs((peak_value[0]-peak_bg[0])+(peak_value[1]-peak_bg[1])))
+### COMMENTED BY ALEX
+# if valley_fitted_value <  valley_bg:
+#    valley_fitted_value = min( projy[valley_array])
+#    if valley_fitted_value <  valley_bg:
+#        raise Exception("valley_fitted_value <  valley_bg")
+#        #return
 
-    idx = 0 if abs(peak_value[0]-peak_bg[0]) > abs(peak_value[1]-peak_bg[1]) else 1
-    ratio_max = (valley_fitted_value-valley_bg)/abs(peak_value[idx]-peak_bg[idx])
+ratio_corrected = 2 * (valley_fitted_value - valley_bg) / (abs((peak_value[0]) + (peak_value[1])))
 
-    emittance = ppol.get_emittance(ratio_corrected)
-    emittance2 = ppol.get_emittance(ratio_max)
+# ratio_corrected = 2*(valley_fitted_value-valley_bg)/(
+#    abs((peak_value[0]-peak_bg[0])+(peak_value[1]-peak_bg[1])))
 
-    _logger.info("ratio=%f emittance %f ratio2=%f emittance2 %f" % (ratio_corrected, emittance, ratio_max, emittance2))
-    return (emittance, emittance2)
+idx = 0 if abs(peak_value[0] - peak_bg[0]) > abs(peak_value[1] - peak_bg[1]) else 1
+ratio_max = (valley_fitted_value - valley_bg) / abs(peak_value[idx] - peak_bg[idx])
 
+emittance = ppol.get_emittance(ratio_corrected)
+emittance2 = ppol.get_emittance(ratio_max)
+
+_logger.debug("ratio=%f emittance %f ratio2=%f emittance2 %f" % (ratio_corrected, emittance, ratio_max, emittance2))
+return (emittance, emittance2)
 
 
 def process_image(image, pulse_id, timestamp, x_axis, y_axis, parameters, bsdata=None):
@@ -225,15 +227,19 @@ def process_image(image, pulse_id, timestamp, x_axis, y_axis, parameters, bsdata
     try:
         ret = calculate_emittance(image, parameters['fit'])
         if ret is not None:
-            emittance,emittance2=ret
+            emittance, emittance2 = ret
 
     except Exception as e:
-         exc_type, exc_obj, exc_tb = sys.exc_info()
-         fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-         status = "Error in " + fname + " line " + str(exc_tb.tb_lineno) + ": " + str(e)
-    ret ={}
+        status = "Error: "
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        while exc_tb is not None:
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            status = status + fname + " line " + str(exc_tb.tb_lineno) + ": " + str(e) + " | "
+            exc_tb = exc_tb.tb_next
+
+    ret = {}
     ret[channel_prefix + ":emmitance"] = emittance
     ret[channel_prefix + ":emmitance2"] = emittance2
-    ret[channel_prefix + ":status"] =  str(status)
+    ret[channel_prefix + ":status"] = str(status)
     return ret
 
